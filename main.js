475b52e599fd99904168e9e0d828d00479e29d58
009kings <009kings@gmail.com>
2021-05-07 01:06:13 -0700

    CONTENT

--- main.js
+++ main.js
@@ -45,15 +45,63 @@ setTimeout(() => {
 }, 5001)
 
 // --------- Why
-const setFilter = (e, obj, displayFunc) => {
-  obj.ref.classList.remove('selected');
-  obj.ref = e.target;
-  e.target.classList.add('selected');
-  obj.name = e.target.innerText;
-  displayFunc();
+const makeSummary = project => {
+  let container = document.createElement('div');
+  container.className = 'experience-list-item';
+  // Header
+  let header = document.createElement('header');
+  header.className = 'experience-list-item-header';
+  header.textContent = project.title;
+  container.append(header);
+  // Summary
+  if (project.summary) {
+    let summary = document.createElement('div');
+    summary.className = 'experience-list-item-summary';
+    summary.textContent = project.summary;
+    container.append(summary);
+  }
+  // Technologies Used
+  let tech = document.createElement('div');
+  tech.className = 'experience-list-item-tech';
+  let techList = project.tech.map(item => {
+    let techContainer = document.createElement('div');
+    techContainer.className = 'experience-list-item-tech-item';
+
+    let techPizazz = document.createElement('div');
+    techPizazz.className = 'experience-list-item-tech-item-pizazz';
+    techContainer.append(techPizazz);
+
+    let techName = document.createElement('p');
+    techName.className = 'experience-list-item-tech-item-name';
+    techName.textContent = item;
+    techContainer.append(techName)
+
+    return techContainer;
+  })
+  techList.forEach(elem => container.append(elem));
+  // Links
+  let links = document.createElement('div');
+  links.className = 'experience-list-item-links';
+
+  let liveLink = document.createElement('a');
+  liveLink.className = `experience-list-item-link ${project.link ? '' : 'disabled'}`;
+  liveLink.href = project.link
+  liveLink.textContent = 'Live Link';
+
+  let github = document.createElement('a');
+  github.className = `experience-list-item-link ${project.github ? '' : 'disabled'}`;
+  github.href = project.github;
+  github.textContent = 'Github Link'
+
+  links.append(liveLink);
+  links.append(github);
+  container.append(links);
+
+  // Return it all
+  return container;
 }
 
-// Populate Skills
+// Populate Fields
 data.skills.forEach(skill => {
   let lang = document.querySelector('.skill-strings-Languages');
   let dbs = document.querySelector('.skill-strings-Databases');
@@ -70,6 +118,31 @@ data.skills.forEach(skill => {
       break;
   }
 })
+
+for (const subj in data.programming) {
+  let expItems = data.programming[subj].map(makeSummary);
+  console.log(expItems)
+  switch (subj) {
+    case 'freelance':
+      expItems.forEach(item => document.querySelector('.experience-list-Freelance').append(item));
+      break;
+    case 'projects':
+      expItems.forEach(item => document.querySelector('.experience-list-Projects').append(item));
+      break;
+    case 'educational':
+      expItems.forEach(item => document.querySelector('.experience-list-Educational').append(item));
+      break;
+  }
+}
+
+// Filter Displays
+const setFilter = (e, obj, displayFunc) => {
+  obj.ref.classList.remove('selected');
+  obj.ref = e.target;
+  e.target.classList.add('selected');
+  obj.name = e.target.innerText;
+  displayFunc();
+}
 
 const displaySkills = () => {
   let lang = document.querySelector('.skill-strings-Languages');
@@ -100,6 +173,38 @@ const displaySkills = () => {
   }
 }
 
+const displayExp = () => {
+  let ed = document.querySelector('.experience-list-Educational');
+  let proj = document.querySelector('.experience-list-Projects');
+  let fl = document.querySelector('.experience-list-Freelance');
+
+  switch (expFilter.name) {
+    case 'Educational':
+      ed.classList.remove('hidden');
+      proj.classList.add('hidden');
+      fl.classList.add('hidden');
+      break;
+    case 'Projects':
+      ed.classList.add('hidden');
+      proj.classList.remove('hidden');
+      fl.classList.add('hidden');
+      break;
+    case 'Freelance':
+      ed.classList.add('hidden');
+      proj.classList.add('hidden');
+      fl.classList.remove('hidden');
+      break;
+    default:
+      ed.classList.remove('hidden');
+      proj.classList.remove('hidden');
+      fl.classList.remove('hidden');
+      break;
+  }
+}
+
+// Populate Experience
+
+
 // Filter click
 let skillFilter = {
   name: 'All',
@@ -112,4 +217,4 @@ let expFilter = {
 
 document.querySelector('.skills .filters-container').addEventListener('click', e => setFilter(e, skillFilter, displaySkills))
 
-document.querySelector('.experience .filters-container').addEventListener('click', e => setFilter(e, expFilter))
+document.querySelector('.experience .filters-container').addEventListener('click', e => setFilter(e, expFilter, displayExp))
